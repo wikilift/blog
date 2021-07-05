4f097ec9a6d480477ba011fbf0f1cea33d7b44c9
wikilift <wikiliftapp@gmail.com>
2021-07-06 01:09:23 +0200

    añadido de menu

---
+++ app/src/main/java/com/wikilift/blogapp/ui/camera/CameraFragment.kt
@@ -0,0 +1,15 @@
+package com.wikilift.blogapp.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wikilift.blogapp.R
+
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/wikilift/blogapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,14 @@
+package com.wikilift.blogapp.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wikilift.blogapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
